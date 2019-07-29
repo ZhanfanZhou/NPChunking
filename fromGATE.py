ce2ba7b5af4d75731eec27d5af45334eb075766e
zhanfan <zhanfanzhou1995@gmail.com>
2019-07-29 17:43:50 -0400

    iidx part

---
+++ fromGATE.py
@@ -0,0 +1,4 @@
+class gte:
+    def __init__(self, exportation_file):
+        self. gate_pos = {}
+        self.gate_token = {}
--- separateOffLang.py
+++ separateOffLang.py
@@ -2,10 +2,11 @@
 import pandas as pd
 
 
-def sep(file_path, raw_tweet=False):
+def sep(file_path, raw_tweet=False, gate=False):
     """
     :param file_path:
     :param raw_tweet: false for tweetlda.py use, true for obtaining raw tweets
+    :param gate output file for gate "tweet_splitting"(from Parsa :D)
     :return:
     """
     df = pd.read_csv(file_path, header=0, sep='\t', dtype={'id': str})
@@ -33,7 +34,7 @@ def sep(file_path, raw_tweet=False):
                         out_GRP.write(row.id+'\t'+row.tweet + '\n')
                     elif row.subtask_c == "OTH":
                         out_OTH.write(row.id+'\t'+row.tweet + '\n')
-    else:
+    elif not gate:
         out_NOT = open('./out_NOT_text.data', 'w', encoding='utf-8')
         out_UNT = open('./out_UNT_text.data', 'w', encoding='utf-8')
         out_TIN = open('./out_TIN_text.data', 'w', encoding='utf-8')
@@ -54,6 +55,27 @@ def sep(file_path, raw_tweet=False):
                         out_GRP.write(row.tweet + '\n')
                     elif row.subtask_c == "OTH":
                         out_OTH.write(row.tweet + '\n')
+    else:
+        out_NOT = open('./out_NOT_gate.data', 'w', encoding='utf-8')
+        out_UNT = open('./out_UNT_gate.data', 'w', encoding='utf-8')
+        out_TIN = open('./out_TIN_gate.data', 'w', encoding='utf-8')
+        out_IND = open('./out_IND_gate.data', 'w', encoding='utf-8')
+        out_GRP = open('./out_GRP_gate.data', 'w', encoding='utf-8')
+        out_OTH = open('./out_OTH_gate.data', 'w', encoding='utf-8')
+        for index, row in df.iterrows():
+            if row.subtask_a == "NOT":
+                out_NOT.write(row.id + '\t' + row.subtask_a + '\t' + row.tweet + '\n')
+            else:
+                if row.subtask_b == "UNT":
+                    out_UNT.write(row.id + '\t' + row.subtask_b + '\t' + row.tweet + '\n')
+                else:
+                    out_TIN.write(row.id + '\t' + row.subtask_b + '\t' + row.tweet + '\n')
+                    if row.subtask_c == "IND":
+                        out_IND.write(row.id + '\t' + row.subtask_c + '\t' + row.tweet + '\n')
+                    elif row.subtask_c == "GRP":
+                        out_GRP.write(row.id + '\t' + row.subtask_c + '\t' + row.tweet + '\n')
+                    elif row.subtask_c == "OTH":
+                        out_OTH.write(row.id + '\t' + row.subtask_c + '\t' + row.tweet + '\n')
 
     print("finished.")
     out_NOT.close()
@@ -69,4 +91,4 @@ def add_headers(outputs):
         output.write('id\ttweet\n')
 
 
-sep('./olid-training-v1.0.tsv', raw_tweet=False)
+sep('./olid-training-v1.0.tsv', raw_tweet=False, gate=True)
